e9d20861a3d9c6d413ed08ea164d799b8b6b9a92
samStan4 <sam.charles.stanley@gmail.com>
2025-02-01 15:02:58 -0800

    removed sh files for Makefile

--- programming-assignments/PA1/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-g++ main.cpp wrapper/application_wrapper.cpp file_readers/gene_sequence_reader.cpp file_readers/alignment_parameters_reader.cpp pairwise_sequence_alignment/pairwise_sequence_alignment.cpp -o exe
-
