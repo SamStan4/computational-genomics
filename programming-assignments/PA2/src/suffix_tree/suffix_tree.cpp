94a54529caf30713f57107ffbf98ceeab2435bfd
samStan4 <sam.charles.stanley@gmail.com>
2025-03-27 13:31:22 -0700

    checkpoint

--- programming-assignments/PA2/src/suffix_tree/suffix_tree.cpp
+++ programming-assignments/PA2/src/suffix_tree/suffix_tree.cpp
@@ -1,19 +1,32 @@
 #include "./suffix_tree.hpp"
 
 void suffix_tree::constructor_helper() {
-    // Hot pointer is the most recently created leaf node in the tree
+    // Hot pointer is the most recently inserted leaf node
     suffix_tree_node* hot_ptr = this->m_root_ptr;
     for (i32 i = 0; i <= (i32)this->m_string.size(); ++i) {
         this->find_path_and_insert(this->m_root_ptr, i, 0, hot_ptr);
         // suffix_tree_node* suffix_link = hot_ptr->m_parent_ptr->m_suffix_ptr;
         // if (suffix_link) {
-        //     // case 1
         //     this->find_path_and_insert(suffix_link, i + suffix_link->m_depth, suffix_link->m_depth, hot_ptr);
         // } else {
-        //     // case 2
+        //     // suffix_link = this->node_hops(hot_ptr->m_parent_ptr->m_parent_ptr);
+        //     // hot_ptr->m_parent_ptr->m_suffix_ptr = suffix_link;
+        //     // this->find_path_and_insert(suffix_link, i + suffix_link->m_depth, suffix_link->m_depth, hot_ptr);
         //     this->find_path_and_insert(this->m_root_ptr, i, 0, hot_ptr);
         // }
     }
+}
+
+suffix_tree_node* suffix_tree::node_hops(suffix_tree_node* start_ptr) {
+    i32 i = start_ptr->m_start;
+    i32 j = start_ptr->m_end;
+    suffix_tree_node* cur_ptr = start_ptr->m_suffix_ptr;
+    i += cur_ptr->m_end - cur_ptr->m_start;
+    while (i < j) {
+        cur_ptr = cur_ptr->get_pointer(this->m_string, i);
+        i += cur_ptr->m_end - cur_ptr->m_start;
+    }
+    return cur_ptr;
 }
 
 void suffix_tree::destructor_helper(suffix_tree_node* cur_ptr) {
@@ -43,37 +56,6 @@ void suffix_tree::find_path_and_insert(suffix_tree_node* cur_ptr, i32 position, 
             this->find_path_and_insert(next_ptr, k, depth + (cur_ptr->m_end - cur_ptr->m_start), hot_ptr);
         }
     } else {
-        // suffix_tree_node* new_internal_node_ptr = new suffix_tree_node();
-        // suffix_tree_node* new_leaf_node_ptr = new suffix_tree_node();
-
-        // new_internal_node_ptr->m_parent_ptr = cur_ptr;
-        // new_leaf_node_ptr->m_parent_ptr = cur_ptr;
-
-        // new_internal_node_ptr->m_start = i;
-        // new_internal_node_ptr->m_end = j;
-        // new_internal_node_ptr->m_depth = cur_ptr->m_depth;
-
-        // new_leaf_node_ptr->m_start = k;
-        // new_leaf_node_ptr->m_end = (i32)this->m_string.size() + 1;
-        // new_leaf_node_ptr->m_depth = depth + ((i32)this->m_string.size() + 1 - position);
-
-        // new_internal_node_ptr->m_child_ptr = cur_ptr->m_child_ptr;
-        // cur_ptr->m_child_ptr = new_internal_node_ptr;
-        // cur_ptr->m_end = i;
-        // cur_ptr->m_depth -= (j - i);
-
-        // new_internal_node_ptr->m_sibling_ptr = new_leaf_node_ptr;
-
-        // suffix_tree_node* grandchild_ptr = new_internal_node_ptr->m_child_ptr;
-
-        // // This is gonna get expensive
-        // while (grandchild_ptr) {
-        //     grandchild_ptr->m_parent_ptr = new_internal_node_ptr;
-        //     grandchild_ptr = grandchild_ptr->m_sibling_ptr;
-        // }
-
-        // hot_ptr = new_leaf_node_ptr;
-
         suffix_tree_node* new_internal_node_ptr = new suffix_tree_node();
         suffix_tree_node* new_leaf_node_ptr = new suffix_tree_node();
 
@@ -83,7 +65,7 @@ void suffix_tree::find_path_and_insert(suffix_tree_node* cur_ptr, i32 position, 
 
         new_internal_node_ptr->m_start = cur_ptr->m_start;
         new_internal_node_ptr->m_end = i;
-        new_internal_node_ptr->m_depth = cur_ptr->m_depth - (j - i);
+        new_internal_node_ptr->m_depth = cur_ptr->m_depth - j + i;
 
         cur_ptr->m_start = i;
 
@@ -206,19 +188,19 @@ i32 suffix_tree::get_number_leaves() {
     return res;
 }
 
-void suffix_tree::get_number_internas_helper(suffix_tree_node* cur_ptr, i32& num_internals) {
+void suffix_tree::get_number_internals_helper(suffix_tree_node* cur_ptr, i32& num_internals) {
     if (!cur_ptr) {
         return;
     } else if (!cur_ptr->is_leaf()) {
         ++num_internals;
     }
 
-    this->get_number_internas_helper(cur_ptr->m_sibling_ptr, num_internals);
-    this->get_number_internas_helper(cur_ptr->m_child_ptr, num_internals);
+    this->get_number_internals_helper(cur_ptr->m_sibling_ptr, num_internals);
+    this->get_number_internals_helper(cur_ptr->m_child_ptr, num_internals);
 }
 
 i32 suffix_tree::get_number_internas() {
     i32 res = 0;
-    this->get_number_internas_helper(this->m_root_ptr, res);
+    this->get_number_internals_helper(this->m_root_ptr, res);
     return res;
 }
--- programming-assignments/PA2/src/suffix_tree/suffix_tree.hpp
+++ programming-assignments/PA2/src/suffix_tree/suffix_tree.hpp
@@ -16,7 +16,8 @@ private:
     void print_tree_dfs_stdout_helper(suffix_tree_node*, std::string&);
     void print_tree_structure_helper(suffix_tree_node*, std::string&, std::string&);
     void get_number_leaves_helper(suffix_tree_node*, i32&);
-    void get_number_internas_helper(suffix_tree_node*, i32&);
+    void get_number_internals_helper(suffix_tree_node*, i32&);
+    suffix_tree_node* node_hops(suffix_tree_node* start_ptr);
 public:
     suffix_tree(const std::string&, const std::string&);
     ~suffix_tree();
